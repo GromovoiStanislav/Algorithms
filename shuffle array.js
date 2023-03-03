ab51c9752f246c124790f92e7079943b192b1471
Gromovoi Stanislav <stasgromovoi@gmail.com>
2023-03-03 17:01:58 +0000

    shuffle array

--- shuffle array.js
+++ shuffle array.js
@@ -1,7 +1,6 @@
 /*Тасование Фишера — Йетса или Тасование Кнута
   — это алгоритм создания случайных перестановок конечного множества*/
 
-
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -9,7 +8,6 @@ function shuffle(array) {
 	// let t = array[i]; array[i] = array[j]; array[j] = t
   }
 }
-
 
 // подсчёт вероятности для всех возможных вариантов
 let count = {
